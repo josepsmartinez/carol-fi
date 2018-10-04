22a7184bf9757cbdcc6313a6c070272c9cccb013
jotapem <jpsmartinez@inf.ufrgs.br>
2018-10-04 11:52:24 -0300

    UPD: meu amigo

--- convs/dft.c
+++ convs/dft.c
@@ -2,9 +2,8 @@
 #include <math.h>
 
 #define PI 3.14159265
-#define N 9 // spatial resolution (input size)
+#define N 6 // spatial resolution (input size)
 #define K 3 // kernel size
-#define M 18 // frequency resolution (encoded size)
 
 void dft(int n_samples, float* x, float* y_sin, float* y_cos) {
   //float y_cos[n_samples] = {0};
@@ -34,39 +33,72 @@ void idft(int n_samples, float* y, float* y_sin, float* y_cos) {
   }
 }
 
+void complex_mul(int n_samples, float* a_real, float* a_im, float* b_real, float* b_im, float* out_real, float* out_im) {
+  for(int n=0; n<n_samples; n++) {
+    out_real[n] = 0;
+    out_real[n] += a_real[n] * b_real[n];
+    out_real[n] -= a_im[n] * b_im[n];
+
+    out_im[n] = 0;
+    out_im[n] += a_im[n] * b_real[n];
+    out_im[n] += a_real[n] * b_im[n];
+  }
+
+}
+
 int main () {
-  float x[N] = {0, 0, -13.89, 1, 14.5567, 0, 0};
-  float y_cos[N] = {0};
-  float y_sin[N] = {0};
+  float x[N] = {2, 1, 2, 1};
+  float fx_cos[N] = {0};
+  float fx_sin[N] = {0};
+  float x_[N] = {0};
+
+  float k[N] = {1, 2, 3};
+  float fk_cos[N] = {0};
+  float fk_sin[N] = {0};
+  float k_[N] = {0};
+
+  float fy_cos[N] = {0};
+  float fy_sin[N] = {0};
   float y_[N] = {0};
    
 
   printf("\nInput \n");
   {
     for(int i=0; i<N; i++) printf("X: %f ", x[i]); printf("\n");
-    for(int i=0; i<N; i++) printf("Y_cos: %f ", y_cos[i]); printf("\n");
-    for(int i=0; i<N; i++) printf("Y_sin: %f ", y_sin[i]); printf("\n");
+    for(int i=0; i<N; i++) printf("K: %f ", k[i]); printf("\n");
   }
 
-  dft(N, x, y_sin, y_cos);
+  // Space -> Frequency
+  dft(N, x, fx_sin, fx_cos);
+  dft(N, k, fk_sin, fk_cos);
 
-  printf("\nEncoded \n");
-  {
-    for(int i=0; i<N; i++) printf("X: %f ", x[i]); printf("\n");
-    for(int i=0; i<M; i++) printf("Y_cos: %f ", y_cos[i]); printf("\n");
-    for(int i=0; i<M; i++) printf("Y_sin: %f ", y_sin[i]); printf("\n");
-  }
-
-  idft(N, y_, y_sin, y_cos);
+  // Point-wise multiplication
+  complex_mul(N, fx_cos, fx_sin, fk_cos, fk_sin, fy_cos, fy_sin);
   
+  printf("\nEncoded \n");
+  /*
+  {
+    for(int i=0; i<N; i++) printf("F(X)_cos: %f ", fx_cos[i]); printf("\n");
+    for(int i=0; i<N; i++) printf("F(X)_sin: %f ", fx_sin[i]); printf("\n");
+    for(int i=0; i<N; i++) printf("F(K)_cos: %f ", fk_cos[i]); printf("\n");
+    for(int i=0; i<N; i++) printf("F(K)_sin: %f ", fk_sin[i]); printf("\n");
+    for(int i=0; i<N; i++) printf("F(Y)_cos: %f ", fy_cos[i]); printf("\n");
+    for(int i=0; i<N; i++) printf("F(Y)_sin: %f ", fy_sin[i]); printf("\n");
+
+  }
+  */
+  
+  // Frequency -> Space
+  idft(N, x_, fx_sin, fx_cos);
+  idft(N, y_, fy_sin, fy_cos);
   printf("\nDecoded \n");
   {
-    for(int i=0; i<N; i++) printf("X : %f ", x[i]); printf("\n");
-    for(int i=0; i<N; i++) printf("Y_: %f ", y_[i]); printf("\n");
+    for(int i=0; i<N; i++) printf("Xf: %f ", x_[i]); printf("\n");
+    for(int i=0; i<N; i++) printf("Yf: %f ", y_[i]); printf("\n");
   }
   
   
   
-  printf("deix \n");
+  printf("\ndeix \n");
   return(0);
 }
