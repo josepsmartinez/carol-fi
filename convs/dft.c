ceb465353a8f4665c6c41594d61b2494db1d43c8
José Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2018-10-08 22:42:52 -0300

    UPD: slight refactor (using real/im instead of cos/sin), using S+K-1 to infer N so input and kernel size might be specified independently

--- convs/dft.c
+++ convs/dft.c
@@ -2,32 +2,25 @@
 #include <math.h>
 
 #define PI 3.14159265
-#define N 6 // spatial resolution (input size)
+#define S 4 // spatial resolution (input size)
 #define K 3 // kernel size
+#define N (S+K-1)
 
-void dft(int n_samples, float* x, float* y_sin, float* y_cos) {
-  //float y_cos[n_samples] = {0};
-  //float y_sin[n_samples] = {0};
-
-  // Space -> Frequency
+void dft(int n_samples, float* x, float* y_real, float* y_im) {
   for(int k=0; k<n_samples; k++) {
     for (int n=0; n<n_samples; n++) {
-      y_cos[k] += x[n] * cos(2*PI*k*n/N);
-    }
-  }
-  for(int k=0; k<n_samples; k++) {
-    for (int n=0; n<n_samples; n++) {
-      y_sin[k] -= x[n] * sin(2*PI*k*n/N);
+      y_real[k] += x[n] * cos(2*PI*k*n/N);
+      y_im[k] -= x[n] * sin(2*PI*k*n/N);
     }
   }
 }
 
-void idft(int n_samples, float* y, float* y_sin, float* y_cos) {
+void idft(int n_samples, float* y, float* y_real, float* y_im) {
 
   for (int n=0; n<n_samples; n++) {
     for(int k=0; k<n_samples; k++) {
-      y[n] += y_cos[k] * cos(2*PI*k*n/N);
-      y[n] -= y_sin[k] * sin(2*PI*k*n/N);
+      y[n] += y_real[k] * cos(2*PI*k*n/N);
+      y[n] -= y_im[k] * sin(2*PI*k*n/N);
     }
     y[n] /= (float) n_samples;
   }
@@ -47,35 +40,38 @@ void complex_mul(int n_samples, float* a_real, float* a_im, float* b_real, float
 }
 
 int main () {
+  // Instantiate input signal (spatial data, vectors for frequency coefficients, reversed spatial data)
   float x[N] = {2, 1, 2, 1};
-  float fx_cos[N] = {0};
-  float fx_sin[N] = {0};
+  float fx_real[N] = {0};
+  float fx_im[N] = {0};
   float x_[N] = {0};
 
+  // Instantiate signal filter (same as input signal)
   float k[N] = {1, 2, 3};
-  float fk_cos[N] = {0};
-  float fk_sin[N] = {0};
+  float fk_real[N] = {0};
+  float fk_im[N] = {0};
   float k_[N] = {0};
 
-  float fy_cos[N] = {0};
-  float fy_sin[N] = {0};
+  // Instantiate filter output (vectors for convolution output as frequency coefficientes, reversed spatial data as final output)
+  float fy_real[N] = {0};
+  float fy_im[N] = {0};
   float y_[N] = {0};
    
 
-  printf("\nInput \n");
+  printf("\nInput\n");
   {
     for(int i=0; i<N; i++) printf("X: %f ", x[i]); printf("\n");
     for(int i=0; i<N; i++) printf("K: %f ", k[i]); printf("\n");
   }
 
   // Space -> Frequency
-  dft(N, x, fx_sin, fx_cos);
-  dft(N, k, fk_sin, fk_cos);
+  dft(N, x, fx_real, fx_im);
+  dft(N, k, fk_real, fk_im);
 
   // Point-wise multiplication
-  complex_mul(N, fx_cos, fx_sin, fk_cos, fk_sin, fy_cos, fy_sin);
+  complex_mul(N, fx_real, fx_im, fk_real, fk_im, fy_real, fy_im);
   
-  printf("\nEncoded \n");
+  printf("\nEncoded\n");
   /*
   {
     for(int i=0; i<N; i++) printf("F(X)_cos: %f ", fx_cos[i]); printf("\n");
@@ -89,9 +85,9 @@ int main () {
   */
   
   // Frequency -> Space
-  idft(N, x_, fx_sin, fx_cos);
-  idft(N, y_, fy_sin, fy_cos);
-  printf("\nDecoded \n");
+  idft(N, x_, fx_real, fx_im);
+  idft(N, y_, fy_real, fy_im);
+  printf("\nDecoded\n");
   {
     for(int i=0; i<N; i++) printf("Xf: %f ", x_[i]); printf("\n");
     for(int i=0; i<N; i++) printf("Yf: %f ", y_[i]); printf("\n");
