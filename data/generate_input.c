d597d6d4236404649009ffb22ecfbee08010847e
José Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2018-10-12 18:07:46 -0300

    UPD: print/persist/read input kernels, srand on main, limiting print limits

--- data/generate_input.c
+++ data/generate_input.c
@@ -4,12 +4,13 @@
 
 #include <limits.h>
 
-#define S 18
+#define S 512
+#define M 100
 #define K 3
 
-void print_matrix(float matrix[S][S]) { /* COPY-PASTE HAHA */
-  for(int y=0; y<S; y++) {
-    for(int x=0; x<S; x++) {
+void print_matrix(float matrix[S][S]) { 
+  for(int y=0; y<15; y++) {
+    for(int x=0; x<15; x++) {
       printf("| %1.4f ", matrix[y][x]);
     }
     printf("|\n");
@@ -17,26 +18,31 @@ void print_matrix(float matrix[S][S]) { /* COPY-PASTE HAHA */
   printf("\n");
 }
 
+void print_kernels(float kernels[M][K]) {
+  for(int m=0; m<10; m++) {
+    for(int k=0; k<K; k++) {
+      printf("| %1.4f ", kernels[m][k]);
+    }
+    printf("|\n");
+  }
+  printf("\n");
+}
+
+float random_float() {
+  return -1. + 2.*rand() / (float) INT_MAX;
+}
 
 void persist_input_matrix() {
   float im_in[S][S] = {0};
-
-  time_t t;
 
   FILE *output_ptr;
   int buffer_i;
   float buffer_f;
 
-  /* */
-  time(&t); srand((unsigned int) t);
-  
-
-  //print_matrix(im_in);
-
   /* fills matrix with random float between (-1,1) */
   for(int y=0; y<S; y++){
     for(int x=0; x<S; x++) {
-      im_in[y][x] = -1. + 2.*rand() / (float) INT_MAX;
+      im_in[y][x] = random_float();
     }
   }
 
@@ -58,13 +64,9 @@ void persist_input_matrix() {
   }
 
   fclose(output_ptr);
-
-  
 }
 
-void read_persisted_matrix() {
-  float im_out[S][S] = {0};
-
+void read_persisted_matrix(float im_out[S][S]) {
   FILE *input_ptr;
   int buffer_i;
   float buffer_f;
@@ -72,24 +74,99 @@ void read_persisted_matrix() {
   /* */
   input_ptr = fopen("new_matrix.bin", "rb");
 
-  // ignored header
+  // ignoring header
   fread(&buffer_i, sizeof(int), 1, input_ptr);
 
   // data
   for(int y=0; y<S; y++){
     for(int x=0; x<S; x++) {
-      fread(&buffer_f, 1, sizeof(float), input_ptr);
+      fread(&buffer_f, sizeof(float), 1, input_ptr);
       im_out[y][x] = buffer_f;
     }
   }
   fclose(input_ptr);
-
-  print_matrix(im_out);
 }
 
+void persist_input_kernels() {
+  float kernels_in[M][K] = {0};
+
+  FILE *output_ptr;
+  int buffer_i;
+  float buffer_f;
+
+  /* fills matrix with random float between (-1,1) */
+  for(int m=0; m<M; m++){
+    for(int k=0; k<K; k++) {
+      kernels_in[m][k] = random_float();
+    }
+  }
+
+  print_kernels(kernels_in);
+  
+  /* write output file */
+  output_ptr = fopen("new_kernels.bin", "wb");
+
+  // header (#kernels, #kernel_size)
+  buffer_i = M;
+  fwrite(&buffer_i, sizeof(int), 1, output_ptr);
+
+  buffer_i = K;
+  fwrite(&buffer_i, sizeof(int), 1, output_ptr);
+
+
+  // data
+  for(int m=0; m<M; m++){
+    for(int k=0; k<K; k++) {
+      buffer_f = kernels_in[m][k];
+      fwrite(&buffer_f, sizeof(float), 1, output_ptr);
+    }
+  }
+
+  fclose(output_ptr);
+
+}
+
+void read_persisted_kernels(float kernels_out[M][K]) {
+  FILE *input_ptr;
+  int buffer_i;
+  float buffer_f;
+
+  input_ptr = fopen("new_kernels.bin", "rb");
+
+  // ignoring header
+  fread(&buffer_i, sizeof(int), 1, input_ptr);
+  fread(&buffer_i, sizeof(int), 1, input_ptr);
+
+  for(int m=0; m<M; m++){
+    for(int k=0; k<K; k++) {
+      fread(&buffer_f, sizeof(float), 1, input_ptr);
+      kernels_out[m][k] = buffer_f;
+    }
+  }
+
+  fclose(input_ptr);
+}
+
+
 int main() {
+  float input_matrix[S][S];
+  float input_kernels[M][K];
+  
+  time_t t;
+  time(&t);
+  srand((unsigned int) t);
+  
+  printf("Persisting input matrix\n");
   persist_input_matrix();
-  read_persisted_matrix();
+  printf("Reading input matrix\n");
+  read_persisted_matrix(input_matrix);
+  print_matrix(input_matrix);
+
+  printf("Persisting input kernels\n");
+  persist_input_kernels();
+  printf("Reading input kernels\n");
+  read_persisted_kernels(input_kernels);
+  print_kernels(input_kernels);
   
   return 0;
 }
