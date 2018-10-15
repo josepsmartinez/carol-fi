1dddac1646bc9025ade1df82edb24490d2fd93ca
José Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2018-10-15 20:05:20 -0300

    UPD: relative pathes for importing sample matrixes ; parameterizing acceptable threshold instead of whole function ; encapsulating tests in main() ; running more tests by varying threshold and comparing a matrix with itself

--- compare_matrix.py
+++ compare_matrix.py
@@ -1,20 +1,28 @@
 import pandas as pd
-m1 = pd.read_csv('/Users/leonardodalcin/Downloads/carol-fi-master/matrix1.txt')
-m2 = pd.read_csv('/Users/leonardodalcin/Downloads/carol-fi-master/matrix2.txt')
+m1 = pd.read_csv('matrix1.txt')
+m2 = pd.read_csv('matrix2.txt')
 
-def compare_matrix(m1,m2,compare_function):
-	for column in m1:
-		for row in m1:
-			m1_number = m1[row][int(column)]
-			m2_number = m2[row][int(column)]
-			if not compare_function(m1_number,m2_number):
-				return False
+def compare_threshold(n1,n2, err):
+        acceptable = abs(n1-n2) <= err
+        return acceptable
 
+
+def compare_matrix(m1,m2,err):
+        for column in m1:
+                for row in m1:
+                        m1_number = m1[row][int(column)]
+                        m2_number = m2[row][int(column)]
+                        if not compare_threshold(m1_number,m2_number,err):
+                                return False
 	return True
 
 
-def compare_threshold(n1,n2):
-	is_different = abs(n1-n2) < 0.3
-	return is_different
+def main():
 
-print(compare_matrix(m1,m2,compare_threshold))
+        print(compare_matrix(m1,m2,3e-1))
+        print(compare_matrix(m1,m2,3e-2))
+        print(compare_matrix(m1,m1,0.0))
+        print(compare_matrix(m2,m2,0.0))
+
+if __name__ == '__main__':
+        main()
