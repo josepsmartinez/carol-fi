fa99fa5caa199eb59e59a567f74dd619260b3b99
José Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2018-10-15 21:14:31 -0300

    UPD: compare_files so a filesystem interface is available

--- compare_matrix.py
+++ compare_matrix.py
@@ -1,13 +1,12 @@
 import pandas as pd
-m1 = pd.read_csv('matrix1.txt')
-m2 = pd.read_csv('matrix2.txt')
+
 
 def compare_threshold(n1,n2, err):
         acceptable = abs(n1-n2) <= err
         return acceptable
 
 
-def compare_matrix(m1,m2,err):
+def compare_matrix(m1, m2, err):
         for column in m1:
                 for row in m1:
                         m1_number = m1[row][int(column)]
@@ -16,13 +15,21 @@ def compare_matrix(m1,m2,err):
                                 return False
 	return True
 
+def compare_files(fn1, fn2, err):
+        m1 = pd.read_csv(fn1)
+        m2 = pd.read_csv(fn2)
+
+        return compare_matrix(m1, m2, err)
+
 
 def main():
+        fn1 = 'matrix1.txt'
+        fn2 = 'matrix2.txt'
 
-        print(compare_matrix(m1,m2,3e-1))
-        print(compare_matrix(m1,m2,3e-2))
-        print(compare_matrix(m1,m1,0.0))
-        print(compare_matrix(m2,m2,0.0))
+        print(compare_files(fn1,fn2,3e-1))
+        print(compare_files(fn1,fn2,3e-2))
+        print(compare_files(fn1,fn1,0.0))
+        print(compare_files(fn2,fn2,0.0))
 
 if __name__ == '__main__':
         main()
