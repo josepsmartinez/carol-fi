ec0a6315c8cf8ac4bc1f3bceab1aa7745793245b
José Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2018-10-16 00:12:45 -0300

    FIX: outputPixel was declared as int ://// ; UPD: loading input and outputing computation on disk, freeing on main, getting rid of some commented prints

--- convs/linear.c
+++ convs/linear.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 #include "malloc2d.h"
+#include "input_synth.h"
 
-#define ImgSize 3
-#define KernelSize 3
-#define NumberOfKernels 3
+#define ImgSize S
+#define KernelSize K
+#define NumberOfKernels M
 
 void simpleConvolution(float** inputImage, float** outputImage, float** convKernel) {
   int imgX = 0, imgY = 0;
@@ -13,14 +14,13 @@ void simpleConvolution(float** inputImage, float** outputImage, float** convKern
   int kernelX = 0, kernelY = 0;
   int kernelMiddle = (int) (KernelSize/2);
 
-  int outputPixel = 0;
+  float outputPixel = 0;
   
   for(imgX;imgX<ImgSize;imgX++){
     imgY = 0;
     for(imgY;imgY<ImgSize;imgY++){
-      //printf("image_x %d , image_y %d \n", imgX, imgY);
-
       outputPixel = 0;
+
       int kernelImgX = imgX - kernelMiddle;
       int kernelImgY = imgY - kernelMiddle;
       //printf("kernel_image_x %d , kernel_image_y %d \n", kernelImgX, kernelImgY);
@@ -28,12 +28,8 @@ void simpleConvolution(float** inputImage, float** outputImage, float** convKern
 
       for(kernelX=0;kernelX<KernelSize;kernelX++){
 	for(kernelY=0;kernelY<KernelSize;kernelY++){
-
-	  //printf("kernel_x %d , kernel_y %d \n", kernelX, kernelY);
-
 	  // arrumar tratamento do kernel
 	  if((kernelX+kernelImgX >= 0 ) && (kernelY+kernelImgX >= 0 ) && (kernelX+kernelImgX) < ImgSize && (kernelY+kernelImgY) < ImgSize){
-	    //printf("%d x %d \t", inputImage[kernelImgX][kernelImgY], convKernel[kernelX][kernelY]);
 	    outputPixel += inputImage[kernelImgX+kernelX][kernelImgY+kernelY] * convKernel[kernelX][kernelY];
 	  }
 	}
@@ -56,27 +52,36 @@ void printImg(float** image) {
 }
 
 int main() {
-  float **inputImage = malloc_2d(ImgSize, ImgSize, 0.0);
-  inputImage[0][0] = 1;
-  inputImage[0][1] = 2;
-  inputImage[0][2] = 3; //
-  inputImage[1][0] = 4;
-  inputImage[1][1] = 5;
-  inputImage[1][2] = 6; //
-  inputImage[2][0] = 7;
-  inputImage[2][1] = 8;
-  inputImage[2][2] = 9;
+  float **inputImage = read_persisted_matrix();
+  float **inputKernels = read_persisted_kernels();
 
-  float **outputImage = malloc_2d(ImgSize, ImgSize, 0.0);
-
-  float **convKernel = malloc_2d(KernelSize, KernelSize, 0.0);
-  convKernel[1][1] = 2;
+  /*
+  printf("Loaded input\n");
+  print_matrix(inputImage, 10, 10);
+  print_matrix(inputKernels, 10, KernelSize*KernelSize);
+  */
   
-  printImg(inputImage);
+  float **convKernel = malloc_2d(KernelSize, KernelSize, 5.0);
+  float **outputImage = malloc_2d(ImgSize, ImgSize, -1.0);
+  
+
+  
+
+  FILE* output_file_ptr = fopen("output.txt", "w");
   
   for (int kernelNumber=0; kernelNumber<NumberOfKernels; kernelNumber++){
+    kernel_matrix_from_line(inputKernels[kernelNumber], convKernel);
+
     simpleConvolution(inputImage, outputImage, convKernel);
-    printImg(outputImage);
+    
+    output_matrix(output_file_ptr, outputImage, ImgSize, ImgSize);
   }
+
+  fclose(output_file_ptr);
+
+  free_2d(ImgSize, inputImage);
+  free_2d(NumberOfKernels, inputKernels);
+  free_2d(ImgSize, outputImage);
+  free_2d(KernelSize, convKernel);
 
 }
