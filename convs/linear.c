8f95258da0dd36f0f6fe21c33e35c2f2b2ecb4e0
José Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2018-11-26 22:44:50 -0200

    __DUPLICATE def on linear ; carolfi_output refs update ; other minor stuff

--- convs/linear.c
+++ convs/linear.c
@@ -3,11 +3,14 @@
 
 #include "malloc2d.h"
 #include "input_synth.h"
+#include "carolfi_output.h"
 
 #define ImgSize S
 #define KernelSize K
 #define NumberOfKernels M
-#define NumberOfThreads 4
+
+#define _DUPLICATE
+
 
 void simpleConvolution(float** inputImage, float** outputImage, float** convKernel) {
   int imgX = 0, imgY = 0;
@@ -28,12 +31,11 @@ void simpleConvolution(float** inputImage, float** outputImage, float** convKern
 
 
       for(kernelX=0;kernelX<KernelSize;kernelX++){
-	for(kernelY=0;kernelY<KernelSize;kernelY++){
-	  // arrumar tratamento do kernel
-	  if((kernelX+kernelImgX >= 0 ) && (kernelY+kernelImgX >= 0 ) && (kernelX+kernelImgX) < ImgSize && (kernelY+kernelImgY) < ImgSize){
-	    outputPixel += inputImage[kernelImgX+kernelX][kernelImgY+kernelY] * convKernel[kernelX][kernelY];
-	  }
-	}
+	       for(kernelY=0;kernelY<KernelSize;kernelY++){
+	          if((kernelX+kernelImgX >= 0 ) && (kernelY+kernelImgX >= 0 ) && (kernelX+kernelImgX) < ImgSize && (kernelY+kernelImgY) < ImgSize){
+	             outputPixel += inputImage[kernelImgX+kernelX][kernelImgY+kernelY] * convKernel[kernelX][kernelY];
+            }
+         }
 		  }
       outputImage[imgX][imgY] = outputPixel;
     }
@@ -52,25 +54,12 @@ void printImg(float** image) {
   printf("\n");
 }
 
-void compare_output(float** image1, float** image2, char* detect_ptr_file) {
-  FILE* fp;
-  int imgX, imgY;
-
-  for (imgX=0; imgX<ImgSize; imgX++)
-  {
-    for(imgY=0; imgY<ImgSize; imgY++)
-      if (image1[imgX][imgY] != image2[imgX][imgY])
-      if (fp = fopen(detect_ptr_file, "a")) {
-        fprintf(fp, "[%d, %d]: %f %f\n", imgX, imgY, image1[imgX][imgY],image2[imgX][imgY]);
-        fclose(fp);
-      }
-  }
-}
-
 int main(int argc, char **argv) {
   float **inputImage;
   float **outputImage1;
-  float **outputImage2;
+  #ifdef _DUPLICATE
+    float **outputImage2;
+  #endif
 
   float **inputKernels;
   float **convKernel;
@@ -87,9 +76,13 @@ int main(int argc, char **argv) {
     inputImage = read_persisted_matrix(argv[1]);
     inputKernels = read_persisted_kernels(argv[2]);
 
-    outputImage1 = malloc_2d(ImgSize, ImgSize, -1.0);
-    outputImage2 = malloc_2d(ImgSize, ImgSize, -1.0);
     convKernel = malloc_2d(KernelSize, KernelSize, 5.0);
+
+    outputImage1 = malloc_2d(ImgSize, ImgSize, -1.0);
+    #ifdef _DUPLICATE
+      outputImage2 = malloc_2d(ImgSize, ImgSize, -1.0);
+    #endif
+
   }
   else {
     printf("Wrong call of script! Usage is '<matrix_file, kernels_file, output_file, detect_file>\n");
@@ -102,10 +95,11 @@ int main(int argc, char **argv) {
 
     simpleConvolution(inputImage, outputImage1, convKernel);
 
+    #ifdef _DUPLICATE
     simpleConvolution(inputImage, outputImage2, convKernel);
 
-
-    compare_output(outputImage1, outputImage2, argv[4]);
+    compare_output(outputImage1, outputImage2, argv[4], S);
+    #endif
 
     output_matrix(output_file_ptr, outputImage1, ImgSize, ImgSize);
   }
@@ -115,8 +109,11 @@ int main(int argc, char **argv) {
   free_2d(ImgSize, inputImage);
   free_2d(NumberOfKernels, inputKernels);
 
-  free_2d(ImgSize, outputImage1);
-  free_2d(ImgSize, outputImage2);
   free_2d(KernelSize, convKernel);
+  free_2d(ImgSize, outputImage1);
+  #ifdef _DUPLICATE
+  free_2d(ImgSize, outputImage2);
+  #endif
+
 
 }
